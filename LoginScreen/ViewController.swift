deea36d7028e14940e1ced19571fa394d9d8ac9b
Training <training@sinatra.fritz.box>
2016-09-07 18:19:52 +0200

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Training on 07/09/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
